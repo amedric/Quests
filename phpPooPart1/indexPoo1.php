e806def182659cdba544f3d52b4d7bb78f5b4975
amedric <105995567+amedric@users.noreply.github.com>
2022-10-10 19:19:01 +0200

    Update indexPoo1.php

--- phpPooPart1/indexPoo1.php
+++ phpPooPart1/indexPoo1.php
@@ -2,22 +2,24 @@
 
 require_once 'classPoo1.php';
 
-$bike = new Bicycle('blue',15,1,2, false);
+$bmx = new Bicycle('blue',15,1,2, false);
 
-echo $bike->forward(15);
-echo '<br> Vitesse du vélo : ' . $bike->currentSpeed . ' km/h' . '<br>';
-echo $bike->brake();
-echo '<br> Vitesse du vélo : ' . $bike->currentSpeed . ' km/h' . '<br>';
-echo $bike->brake();
+echo $bmx->forward(15);
+echo '<br> Vitesse du vélo : ' . $bmx->getcurrentSpeed() . ' km/h' . '<br>';
+echo $bmx->brake();
+echo '<br> Vitesse du vélo : ' . $bmx->getcurrentSpeed() . ' km/h' . '<br>';
+echo $bmx->brake();
 
 
 
-$car = new Cars('yellow', 5, 'petrol');
+$ferrari = new Cars('yellow', 5, 'petrol');
 
-echo $car->carForward(25);
-echo '<br> Vitesse du vélo : ' . $car->currentCarSpeed . ' km/h' . '<br>';
-echo $car->carBrake();
-echo '<br> Vitesse du vélo : ' . $car->currentCarSpeed . ' km/h' . '<br>';
-echo $car->carBrake();
+echo $ferrari->carStart();
+echo '<br> Vitesse de la voiture : ' . $ferrari->getCurrentSpeed() . ' km/h' . '<br>';
+echo $ferrari->carForward(25);
+echo '<br> Vitesse de la voiture : ' . $ferrari->getCurrentSpeed() . ' km/h' . '<br>';
+echo $ferrari->carBrake();
+echo '<br> Vitesse de la voiture : ' . $ferrari->getCurrentSpeed() . ' km/h' . '<br>';
+echo $ferrari->carBrake();
 
 ?>
