02a585908b96317a7026e8ce40009899ded454cb
amedric <105995567+amedric@users.noreply.github.com>
2022-11-02 20:15:42 +0100

    Update car.php

--- phpPooPart4/car.php
+++ phpPooPart4/car.php
@@ -1,53 +1,26 @@
 <?php
+require_once 'Vehicle.php';
 
-class Car
+class Car extends Vehicle
 {
-    private bool $hasParkBrake;
+    private bool $hasParkBrake=true;
 
-    public function __construct(bool $hasParkBrake)
+    public function setParkBrake(bool $boolean): void
     {
-        $this->hasParkBrake = $hasParkBrake;
+        $this->hasParkBrake = $boolean;
+    }
+
+    public function getParkBrake()
+    {
+        $this->hasParkBrake;
     }
 
     public function start()
     {
-
-        try {
-
-            echo division(5) . PHP_EOL;
-        
-            echo division(0) . PHP_EOL;
-        
-        } catch(Exception $e){
-        
-            // code to manage exceptions
-        
-            echo 'Exception received  : ' . $e->getMessage() . PHP_EOL;
-        
-        } finally {
-        
-            echo "End !" . PHP_EOL;
-        
+        if ($this->hasParkBrake === true) {
+            throw new Exception ('You forgot the handbrake');
         }
-    }
+        $this->goForward();
 
-    /**
-     * Get the value of hasParkBrake
-     */ 
-    public function getHasParkBrake()
-    {
-        return $this->hasParkBrake;
-    }
-
-    /**
-     * Set the value of hasParkBrake
-     *
-     * @return  self
-     */ 
-    public function setParkBrake($hasParkBrake)
-    {
-        $this->hasParkBrake = $hasParkBrake;
-
-        return $this;
     }
 }
